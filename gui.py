687fb77be8d05339af3983a8e81b006d5eac58f4
arikaplan <akkaplan1991@gmail.com>
2017-01-27 17:02:40 -0800

    starting gui

--- gui.py
+++ gui.py
@@ -1,13 +1,49 @@
 from tkinter import *
 
-#creates a blank window
+
+class ArisButtons:
+	def __init__(self, master):
+
+		topframe = Frame(master)
+		topframe.pack()
+
+		bottomframe = Frame(master)
+		bottomframe.pack(side=BOTTOM)
+
+
+		self.label_1 = Label(topframe, text='number 1')
+		self.label_1.grid(row = 0, column = 0)
+		self.label_2 = Label(topframe, text='number 2')
+		self.label_2.grid(row = 1, column = 0)
+
+		#user input
+		self.entry_1 = Entry(topframe)
+		self.entry_1.grid(row = 0, column = 1)
+		self.entry_2 = Entry(topframe)
+		self.entry_2.grid(row = 1, column = 1)
+
+		'''
+		text = Text(topframe)
+		text.pack(side=BOTTOM)
+		output = printMessage(self)
+		text.insert(output)
+		'''
+
+		self.printButton = Button(bottomframe, 
+			text='Add numbers', 
+			command=self.printMessage)
+		self.printButton.pack(side=LEFT)
+
+		self.quitButton = Button(bottomframe, text='quit', command=master.quit)
+		self.quitButton.pack(side=LEFT)
+
+	def printMessage(self):
+		n1 = float(self.entry_1.get())
+		n2 = float(self.entry_2.get())
+		return n1 + n2
+
 root = Tk()
 
-#put text on a screen
-theLabel = Label(root, text='easy')
+b = ArisButtons(root)
 
-#where to put the window, pack just puts it somewhere
-theLabel.pack()
-
-#keeps the window running, rather than shutting down right away
 root.mainloop()
