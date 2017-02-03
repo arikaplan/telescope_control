02d45680288a9dba6bff955278f0ed5578fdc81f
arikaplan <akkaplan1991@gmail.com>
2017-02-03 12:59:33 -0800

    updated notes

--- gui.py
+++ gui.py
@@ -5,12 +5,10 @@ sys.path.append('C:/Python27x86/lib/site-packages')
 import gclib
 from tkinter import *
 
-
 #make an instance of the gclib python class
 g = gclib.py()
 #connect to network
 g.GOpen('10.1.2.245 --direct -s ALL')
-#
 #g.GOpen('COM1 --direct')
 #used for galil commands
 c = g.GCommand
@@ -20,47 +18,55 @@ c('MO') #turn off all motors
 c('SH') #servo on
 
 
+
 class interface:
+
 	#init gets called automatically
 	def __init__(self, master):
+
 
 		topframe = Frame(master)
 		topframe.pack()
 
-		bottomframe = Frame(master)
-		bottomframe.pack(side=BOTTOM)
+		inputfame = Frame(master)
+		inputfame.pack(side=TOP)
 
+		buttonframe = Frame(master)
+		buttonframe.pack(side=BOTTOM)
 
-		self.l1 = Label(topframe, text='scan time (seconds)')
+		self.title = Label(topframe, text = 'Az Scan')
+		self.title.pack()
+
+		self.l1 = Label(inputfame, text='scan time (seconds)')
 		self.l1.grid(row = 0, column = 0, sticky=W)
-		self.l2 = Label(topframe, text='iteration #')
+		self.l2 = Label(inputfame, text='iteration #')
 		self.l2.grid(row = 1, column = 0, sticky=W)
-		self.l3 = Label(topframe, text='El Step Size (deg)')
+		self.l3 = Label(inputfame, text='El Step Size (deg)')
 		self.l3.grid(row = 2, column = 0, sticky=W)
-		self.l4 = Label(topframe, text='starting az (deg)')
+		self.l4 = Label(inputfame, text='starting az (deg)')
 		self.l4.grid(row = 3, column = 0, sticky=W)
-		self.l5 = Label(topframe, text='starting el (deg)')
+		self.l5 = Label(inputfame, text='starting el (deg)')
 		self.l5.grid(row = 4, column = 0, sticky=W)
 
 		#user input
-		self.tscan = Entry(topframe)
+		self.tscan = Entry(inputfame)
 		self.tscan.grid(row = 0, column = 1)
-		self.iterations = Entry(topframe)
+		self.iterations = Entry(inputfame)
 		self.iterations.grid(row = 1, column = 1)
-		self.deltaEl = Entry(topframe)
+		self.deltaEl = Entry(inputfame)
 		self.deltaEl.grid(row = 2, column = 1)
-		self.az0 = Entry(topframe)
+		self.az0 = Entry(inputfame)
 		self.az0.grid(row = 3, column = 1)
-		self.el0 = Entry(topframe)
+		self.el0 = Entry(inputfame)
 		self.el0.grid(row = 4, column = 1)
 
 
-		self.scan = Button(bottomframe, 
+		self.scan = Button(buttonframe, 
 			text='Start Scan', 
 			command=self.scanAz)
 		self.scan.pack(side=LEFT)
 
-		self.quitButton = Button(bottomframe, text='quit', command=master.quit)
+		self.quitButton = Button(buttonframe, text='quit', command=master.quit)
 		self.quitButton.pack(side=LEFT)
 
 
@@ -74,6 +80,8 @@ class interface:
 
 		scan.azScan(tscan, iterations, az0, el0, deltaEl, c)
 
+		g.GClose() #close connections
+
 
 root = Tk()
 
@@ -81,4 +89,3 @@ b = interface(root)
 
 root.mainloop()
 
-g.GClose() #close connections
